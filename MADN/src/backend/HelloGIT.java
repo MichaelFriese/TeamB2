3c60e556b40576fd30eb88df55591330a0e8451b
dortepuchelt <dortepuchelt@dortepuheltsmbp.fh-reutlingen.de>
2015-03-13 11:13:57 +0100

    Test des hochladens

--- MADN/src/backend/HelloGIT.java
+++ MADN/src/backend/HelloGIT.java
@@ -7,6 +7,7 @@ public class HelloGIT {
 		System.out.println("Hallo vom ersten!");
 		System.out.println("Hallo vom zweiten!");
 		System.out.println("Hallo 3");
+		System.out.println("Hallo ich Teste es auch mal");
 
 	}
 
