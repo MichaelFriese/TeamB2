06e7a4baeab58dc03d0b307a7e142e6880a67986
dortepuchelt <dortepuchelt@dortepuheltsmbp.fh-reutlingen.de>
2015-03-17 12:44:12 +0100

    Großbuchstaben verändert

--- MADN/src/backend/Spielbrett.java
+++ MADN/src/backend/Spielbrett.java
@@ -10,28 +10,28 @@ public class Spielbrett {
 		for (int i = 0; i < spielbrett.length; i++) {
 			switch (i + 1) {
 			case (1):
-				spielbrett[i] = new FeldWrapper(true, false, FarbEnum.red);
+				spielbrett[i] = new FeldWrapper(true, false, FarbEnum.RED);
 			break;
 			case (11):
-				spielbrett[i] = new FeldWrapper(true, false,FarbEnum.blue);
+				spielbrett[i] = new FeldWrapper(true, false,FarbEnum.BLUE);
 			break;
 			case (21):
-				spielbrett[i] = new FeldWrapper(true, false,FarbEnum.yellow);
+				spielbrett[i] = new FeldWrapper(true, false,FarbEnum.YELLOW);
 			break;
 			case (31):
-				spielbrett[i] = new FeldWrapper(true, false,FarbEnum.green);
+				spielbrett[i] = new FeldWrapper(true, false,FarbEnum.GREEN);
 			break;
 			case (10):
-				spielbrett[i] = new FeldWrapper(false, true,FarbEnum.blue);
+				spielbrett[i] = new FeldWrapper(false, true,FarbEnum.BLUE);
 			break;
 			case (20):
-				spielbrett[i] = new FeldWrapper(false, true,FarbEnum.green);
+				spielbrett[i] = new FeldWrapper(false, true,FarbEnum.GREEN);
 			break;
 			case (30):
-				spielbrett[i] = new FeldWrapper(false, true,FarbEnum.yellow);
+				spielbrett[i] = new FeldWrapper(false, true,FarbEnum.YELLOW);
 			break;
 			case (40):
-				spielbrett[i] = new FeldWrapper(false, true,FarbEnum.red);
+				spielbrett[i] = new FeldWrapper(false, true,FarbEnum.RED);
 			break;
 			default:
 				spielbrett[i] = new FeldWrapper(false, false,null);
