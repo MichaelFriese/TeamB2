f301a67681c707a9aa63b2e077b7c1e82e0b2326
Judith <judith.waeschle@student.reutlingen-university.de>
2015-04-21 14:42:33 +0200

    KI tests

--- MADN/src/backend/SpielTest.java
+++ MADN/src/backend/SpielTest.java
@@ -19,16 +19,16 @@ public class SpielTest {
 	public static void main(String[] args) {
 		
 		iBediener s = new Spiel ();
-	//	s.SpielerHinzufuegen("Ki", "red", "aggressiv");
+		s.SpielerHinzufuegen("Ki", "red", "aggressiv");
 //		s.SpielerHinzufuegen("Ki2", "Blue", "defensiv");
-		s.SpielerHinzufuegen("Rosa", "red",null);
-		s.SpielerHinzufuegen("Helmut", "blue",null);
+//		s.SpielerHinzufuegen("Rosa", "red",null);
+//		s.SpielerHinzufuegen("Helmut", "blue",null);
 		s.SpielerHinzufuegen("Reiner", "Green",null);
-		s.SpielerHinzufuegen("Ole", "Yellow",null);
-		s.initSpiel();
-		s.zugDurchfuehren(1);
-		s.zugDurchfuehren(1);
-		s.zugDurchfuehren(1);
+//		s.SpielerHinzufuegen("Ole", "Yellow",null);
+//		s.initSpiel();
+//		s.zugDurchfuehren(1);
+//		s.zugDurchfuehren(1);
+//		s.zugDurchfuehren(1);
 //		s.zugDurchfuehren(1);
 //		s.zugDurchfuehren(1);
 //		s.zugDurchfuehren(1);
@@ -39,7 +39,7 @@ public class SpielTest {
 		
 		
 		
-	//	s.initTestSpiel();
+		s.initTestSpiel();
 		
 //		s.initTestSpielZugDefensiv();
 //		s.initTestSpielZugAggressiv();
